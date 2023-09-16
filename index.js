516c890e2d8cfb6676b1416adb7661467e2d978d
b-ez47 <noreply>
2023-09-15 21:37:46 -0400

    remove counter

--- index.js
+++
@@ -1,7 +0,0 @@
-var counterContainer = document.getElementById("website-counter");
-
-var visitCount = localStorage.getItem("page_view");
-visitCount = Number(visitCount) + 1;
-localStorage.setItem("page_view", 1);
-
-counterContainer.innerHTML = visitCount;
